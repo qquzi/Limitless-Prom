ae5cbd2a0c295a9ce4088aa2ad813426c094c316
qquzi <anonymoususer29211@gmail.com>
2026-07-07 11:04:26 -0700

    Update namegenerators.lua

--- src/prometheus/namegenerators.lua
+++ src/prometheus/namegenerators.lua
@@ -1,13 +1,31 @@
--- This Script is Part of the Prometheus Obfuscator by levno-710
---
--- namegenerators.lua
---
--- This Script provides a collection of name generators for Prometheus.
-
-return {
-	Mangled = require("prometheus.namegenerators.mangled");
-	MangledShuffled = require("prometheus.namegenerators.mangled_shuffled");
-	Il = require("prometheus.namegenerators.Il");
-	Number = require("prometheus.namegenerators.number");
-	Confuse = require("prometheus.namegenerators.confuse");
+local generators = {
+    Mangled = require("Prometheus.namegenerators.mangled"),
+    MangledShuffled = require("Prometheus.namegenerators.mangled_shuffled"),
+    Il = require("Prometheus.namegenerators.Il"),
+    Number = require("Prometheus.namegenerators.number"),
+    Confuse = require("Prometheus.namegenerators.confuse")
 }
+
+function generators.get(name)
+    return generators[name]
+end
+
+function generators.exists(name)
+    return generators[name] ~= nil
+end
+
+function generators.list()
+    local list = {}
+
+    for name, generator in pairs(generators) do
+        if type(generator) ~= "function" then
+            list[#list + 1] = name
+        end
+    end
+
+    table.sort(list)
+
+    return list
+end
+
+return generators
